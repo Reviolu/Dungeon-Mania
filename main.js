d77ef317a196ba59aacaae73386695dd13e920e5
oliver <oliverluan04@gmail.com>
2026-03-30 19:08:06 +1100

    improved animation for when facing the left side

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 var config = {
     type: Phaser.AUTO,
-    width: 1440,
+    width: 1880,
     height: 900,
     physics: {
         default: 'arcade',
@@ -46,7 +46,8 @@ function preload () {
 
 function create() {
     player = this.physics.add.sprite(720, 450, 'player_idle', 0)
-    cursors = this.input.keyboard.createCursorKeys();    
+    cursors = this.input.keyboard.createCursorKeys();
+    let facingRight = true; 
     this.anims.create({
         key: 'left',
         frames: this.anims.generateFrameNumbers('player_run_left', {start: 0, end: 5}),
@@ -62,15 +63,29 @@ function create() {
     })
 
     this.anims.create({
-        key: 'up',
+        key: 'up_right',
         frames: this.anims.generateFrameNumbers('player_run_right', {start: 4, end: 5}),
         frameRate: 10,
         repeat: -1
     })
 
     this.anims.create({
-        key: 'down',
+        key: 'down_right',
         frames: this.anims.generateFrameNumbers('player_run_right', {start: 4, end: 5}),
+        frameRate: 10,
+        repeat: -1
+    })
+
+    this.anims.create({
+        key: 'up_left',
+        frames: this.anims.generateFrameNumbers('player_run_left', {start: 4, end: 5}),
+        frameRate: 10,
+        repeat: -1
+    })
+
+    this.anims.create({
+        key: 'down_left',
+        frames: this.anims.generateFrameNumbers('player_run_left', {start: 4, end: 5}),
         frameRate: 10,
         repeat: -1
     })
@@ -86,27 +101,50 @@ function create() {
         frames: this.anims.generateFrameNumbers('player_idle_left', {start: 0, end: 5}),
         frameRate: 10,
     })
-
 }
 
+let facingRight = true;
 function update() {
+
     if (cursors.left.isDown) {
         player.setVelocityX(-160);
         player.anims.play('left', true);
+        facingRight = false;
+        console.log(facingRight);
     } else if (cursors.right.isDown) {
         player.setVelocityX(160);
         player.anims.play('right', true);
+
+        facingRight = true;
+        console.log("right button,", facingRight);
     } 
     else if (cursors.up.isDown) {
         player.setVelocityY(-160);
-        player.anims.play('up', true)
+
+        if (facingRight) {
+            player.anims.play('up_right', true);
+        } else {
+            player.anims.play('up_left', true);
+        }
+        
     } else if (cursors.down.isDown) {
         player.setVelocityY(160);
-        player.anims.play('down', true)
+        if (facingRight) {
+            player.anims.play('down_right', true);
+        } else {
+            player.anims.play('down_left', true);
+        }
     } else {
         player.setVelocityX(0);
         player.setVelocityY(0);
-        player.anims.play('idle_right', true)
+        console.log("idle", facingRight);
+        if (facingRight == true) {
+            player.anims.play('idle_right', true)
+        } else {
+            player.anims.play('idle_left', true);
+        }
+
+
     }
 
 
