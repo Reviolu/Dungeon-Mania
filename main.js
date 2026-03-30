2dd2a2239030542520a0e525131632770154b222
oliver <oliverluan04@gmail.com>
2026-03-30 19:16:17 +1100

    changed folder name in assets folder

--- main.js
+++ main.js
@@ -16,29 +16,26 @@ var config = {
     }
 };
 
-
 var player;
 var game = new Phaser.Game(config)
 
-
-
 function preload () {
-    this.load.spritesheet('player_run_right', "assets/images/player_run_right.png", {
+    this.load.spritesheet('player_run_right', "assets/player/player_run_right.png", {
         frameWidth: 192,
         frameHeight: 192
     });
 
-    this.load.spritesheet('player_run_left', "assets/images/player_run_left.png", {
+    this.load.spritesheet('player_run_left', "assets/player/player_run_left.png", {
     frameWidth: 192,
     frameHeight: 192
     });
 
-    this.load.spritesheet('player_idle_right', "assets/images/player_idle_right.png", {
+    this.load.spritesheet('player_idle_right', "assets/player/player_idle_right.png", {
         frameWidth: 192,
         frameHeight: 192
     });
 
-    this.load.spritesheet('player_idle_left', "assets/images/player_idle_left.png", {
+    this.load.spritesheet('player_idle_left', "assets/player/player_idle_left.png", {
         frameWidth: 192,
         frameHeight: 192
     });
